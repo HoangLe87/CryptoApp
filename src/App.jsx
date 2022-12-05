6706925f87f43e8c2843c3f1a3b8428bcf50226e
HoangLe87 <hongacek@hotmail.com>
2022-12-05 09:50:18 +0100

    getMood and setMood functions done

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,151 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import { ethers } from "ethers";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [currentMood, setCurrentMood] = useState("");
+  const [currentAccount, setCurrentAccount] = useState("");
+  const [inputText, setInputText] = useState("");
+
+  const contractAddress = "0x4DD721098E76b43Bb6772E9Fa61BF8430A0CCc0f";
+  const moodABI = [
+    {
+      inputs: [
+        {
+          internalType: "string",
+          name: "_mood",
+          type: "string",
+        },
+      ],
+      name: "setMood",
+      outputs: [],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "getMood",
+      outputs: [
+        {
+          internalType: "string",
+          name: "",
+          type: "string",
+        },
+      ],
+      stateMutability: "view",
+      type: "function",
+    },
+  ];
+
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { ethereum } = window;
+      if (!ethereum) {
+        console.log("Please install metamask");
+        return;
+      } else {
+        console.log("we have object ethereum", ethereum);
+      }
+
+      const accounts = await ethereum.request({ method: "eth_accounts" });
+
+      if (accounts.length !== 0) {
+        const account = accounts[0];
+        setCurrentAccount(account);
+      } else {
+        console.log("no authorised accounts found");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const getBlockchainMood = async () => {
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum, "goerli");
+        const signer = provider.getSigner();
+        const moodContract = new ethers.Contract(
+          contractAddress,
+          moodABI,
+          signer
+        );
+        const blockchainMood = await moodContract.getMood();
+        setCurrentMood(blockchainMood);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const setBlockchainMood = async () => {
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum, "goerli");
+        const signer = provider.getSigner();
+        const moodContract = new ethers.Contract(
+          contractAddress,
+          moodABI,
+          signer
+        );
+        await moodContract.setMood(inputText);
+        setCurrentMood(inputText);
+        setInputText("");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
 
   return (
     <div className="App">
+      <h1>Write data to the blockchain!</h1>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <p>Get or set the current mood: </p>
+        <label htmlFor="mood">Input moood: </label>
+        <input
+          id="mood"
+          type="text"
+          onChange={(e) => setInputText(e.target.value)}
+          value={inputText}
+        ></input>
+        <div>
+          <button onClick={setBlockchainMood}>Set Mood</button>
+        </div>
+        <div>
+          <button onClick={getBlockchainMood}>Get Mood</button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div>
+        {!currentAccount && (
+          <button onClick={connectWallet}>Connect Wallet</button>
+        )}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>{currentMood && <div>Current mood is {currentMood} </div>}</div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
